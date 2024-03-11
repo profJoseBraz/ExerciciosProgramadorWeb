de202201cc4603a0540e5b2e694c8f0808a7118d
Jose Augusto Braz <jose.braz@docente.pr.senac.br>
2024-03-11 14:04:59 -0300

    adicionando MyInput exemplo

---
+++ React/exemplo-react/src/assets/components/MyInput.tsx
@@ -0,0 +1,19 @@
+import './MyInput.css'
+
+interface Props{
+    type: string;
+    placeholder?: string;
+    bgColor?:string;
+}
+
+function MyInput(props : Props){
+    return(
+        <input 
+        className="my-input" 
+        type={props.type} 
+        placeholder={props.placeholder} 
+        style={{backgroundColor: `${props.bgColor}`}}/>
+    );
+}
+
+export default MyInput
